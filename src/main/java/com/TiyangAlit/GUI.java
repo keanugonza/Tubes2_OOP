fc72bfa6dbce849ee8ef9f114c8fd258bb66f812
RiciTrisnaP <ricitrisnap@gmail.com>
2024-05-21 15:50:28 +0700

    feat:scenebuilder integration

--- src/main/java/com/TiyangAlit/GUI.java
+++ src/main/java/com/TiyangAlit/GUI.java
@@ -1,7 +1,9 @@
 package com.TiyangAlit;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
@@ -9,6 +11,7 @@ import javafx.stage.Stage;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+
 
 public class GUI  extends Application{
 
@@ -19,13 +22,14 @@ public class GUI  extends Application{
     @Override
     public void start(Stage stage) throws Exception {
         try{
-            Group root = new Group();
-            Scene scene = new Scene(root, Color.GRAY);
+            InputStream fxmlUrl = new FileInputStream("src/GUI.fxml");
             InputStream stream  = new FileInputStream("src/StardewValley.png");
             Image image = new Image(stream);
             stage.getIcons().add(image);
-            stage.setScene(scene);
-            stage.setTitle("TyangAlit");
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            Parent root = fxmlLoader.load(fxmlUrl);
+            stage.setTitle("Stardew Valley");
+            stage.setScene(new Scene(root));
             stage.show();
         }catch (Exception e){
             throw new Exception("ERROR");
