e0fe53e4074a2fbd850a2bac77831090136e3017
Keanu Amadius Gonza <140991645+keanugonza@users.noreply.github.com>
2024-05-21 22:27:45 +0700

    feat: Toko

--- src/main/java/com/TiyangAlit/Main.java
+++ src/main/java/com/TiyangAlit/Main.java
@@ -7,11 +7,8 @@ import com.TiyangAlit.Factory.KartuFactory;
 import com.TiyangAlit.Factory.ProdukFactory.JenisProdukFactory.ProdukHewanFactory;
 import com.TiyangAlit.Kartu.Entity.Hewan.Jenis.Karnivora;
 import com.TiyangAlit.Kartu.Item.Item;
-import com.TiyangAlit.Kartu.Kartu;
 import com.TiyangAlit.Kartu.Produk.JenisProduk.ProdukHewan;
-import com.TiyangAlit.Kartu.Produk.Produk;
 import com.TiyangAlit.Player.Player;
-import com.TiyangAlit.Toko.Toko;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -24,24 +21,51 @@ import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        // Bikin factory
-        KartuFactory produkHewanFactory = new ProdukHewanFactory();
-        KartuFactory karnivoraFactory = new KarnivoraFactory();
-        KartuFactory itemFactory = new ItemFactory();
-
-        // Bikin kartu
-        ProdukHewan susu = (ProdukHewan) produkHewanFactory.createKartu("Susu");
-        Toko toko = new Toko();
-        try {
-            toko.tambahKartu(susu);
-            toko.printMapData();
-            toko.ambilKartu(susu);
-            toko.printMapData();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-//        Path currRelativePath = Paths.get("");
-//        String currAbsolutePathString = currRelativePath.toAbsolutePath().toString();
-//        System.out.println("Current absolute path is - " + currAbsolutePathString);
+//        Player player1 = new Player("player1");
+//
+//        // Bikin factory
+//        KartuFactory produkHewanFactory = new ProdukHewanFactory();
+//        KartuFactory karnivoraFactory = new KarnivoraFactory();
+//        KartuFactory itemFactory = new ItemFactory();
+//
+//        // Bikin kartu
+//        ProdukHewan susu = (ProdukHewan) produkHewanFactory.createKartu("Susu");
+//        Karnivora hiuDarat = (Karnivora) karnivoraFactory.createKartu("Hiu Darat");
+//        Item accelerate = (Item) itemFactory.createKartu("Accelerate");
+//        Item delay = (Item) itemFactory.createKartu("Delay");
+//        Item instantHarvest = (Item) itemFactory.createKartu("Instant Harvest");
+//        Item destroy = (Item) itemFactory.createKartu("Destroy");
+//        Item protect = (Item) itemFactory.createKartu("Protect");
+//        Item trap = (Item) itemFactory.createKartu("Trap");
+//
+//        try {
+//            player1.place(0, 0, hiuDarat);  // Hiu1
+//            player1.place(1, 0, hiuDarat);  // Hiu2
+//            player1.getLadang().displayLadang();
+//            for (int i = 0; i < 7; i++)
+//                player1.place(0, 0, susu);  // Kasih 7 susu ke Hiu1
+//            player1.place(1, 0, accelerate);    // Accelerate hiu2
+//            player1.place(1, 0, protect);       // Protect hiu2
+//            player1.place(1, 0, destroy);       // Destroy hiu2 (gagal karena ada protect)
+//            player1.place(1, 0, delay);         // Delay hiu2
+//            player1.place(1, 0, trap);          // Trap hiu2
+//
+//            System.out.println("Berat Hiu1: " + player1.getLadang().getData().getEl(0, 0).getBobot());
+//            System.out.println("Berat Hiu2: " + player1.getLadang().getData().getEl(1, 0).getBobot());
+//            System.out.println("Efek Hiu2: " + player1.getLadang().getData().getEl(1, 0).getEffects());
+//
+//            player1.panen(0,0); // Panen Hiu1
+//
+//            player1.getDeckAktif().displayDeck();
+//            player1.place(1, 0, instantHarvest); // Panen Hiu2
+//            player1.getDeckAktif().displayDeck();
+//
+//            player1.getLadang().displayLadang();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        Path currRelativePath = Paths.get("");
+        String currAbsolutePathString = currRelativePath.toAbsolutePath().toString();
+        System.out.println("Current absolute path is - " + currAbsolutePathString);
     }
 }
